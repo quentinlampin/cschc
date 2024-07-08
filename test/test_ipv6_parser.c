5e42f70b54f1bcf54676a61b28cf256978b25584
Corentin Banier <corentinbanier@protonmail.com>
2024-07-08 15:31:29 +0200

    test(parser): Update old tests to match the current representation of IPv6 and UDP headers.

--- test/test_ipv6_parser.c
+++ test/test_ipv6_parser.c
@@ -1,71 +1,120 @@
-#include <stdlib.h>
-#include <string.h>
+#include "parser/parser.h"
+
 #include <assert.h>
+#include <stdio.h>
+#include <string.h>
 
-#include "ipv6.h"
+/* ********************************************************************** */
 
-#define TEST_SUCCESS 0
+void test_unpack_ver_tc_fl(void) {
+  const uint32_t packed_value          = 0b01101111111110011010101011110010;
+  const uint8_t  ipv6_expected_version = 0b0110;
+  const uint8_t  ipv6_expected_traffic_class = 0b11111111;
+  const uint32_t ipv6_expected_flow_label    = 0b10011010101011110010;
 
+  uint8_t  ipv6_version, ipv6_traffic_class;
+  uint32_t ipv6_flow_label;
 
-int test_ipv6_parser(){
-    /**
-     * @brief testing the IPv6 parser on an IPv6 header
-     * 
-     * The packet is made of an IPv6 header with following fields:
-     *  - id='Version'              length=4    position=0  value={0x06}
-     *  - id='Traffic Class'        length=8    position=0  value={0x00}
-     *  - id='Flow Label'           length=20   position=0  value={0x00, 0x00, 0x00}
-     *  - id='Payload Length'       length=16   position=0  value={0x10}
-     *  - id='Next Header'          length=8    position=0  value={0x11}
-     *  - id='Hop Limit'            length=8    position=0  value={0x40}
-     *  - id='Source Address'       length=128  position=0  value={0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa1}
-     *  - id='Destination Address'  length=128  position=0  value={0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa2}
-     * 
-     */
-    const unsigned char buffer_content_ipv6_header[] = {
-        0x60, 0x00, 0x00, 0x00, 0x00, 0x10, 0x11, 0x40, 0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa1, 0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa2, 0x23, 0x29, 0x23, 0x2a, 0x00, 0x10, 0x2d, 0xa1,
-        0x64, 0x65, 0x61, 0x64, 0x62, 0x65, 0x65, 0x66
-    };
+  unpack_ver_tc_fl(&ipv6_version, &ipv6_traffic_class, &ipv6_flow_label,
+                   packed_value);
 
-    const unsigned char buffer_content_version[] = {0x06};
-    const unsigned char buffer_content_traffic_class[] = {0x00};
-    const unsigned char buffer_content_flow_label[] = {0x00, 0x00, 0x00};
-    const unsigned char buffer_content_payload_length[] = {0x00, 0x10};
-    const unsigned char buffer_content_next_header[] = {0x11};
-    const unsigned char buffer_content_hop_limit[] = {0x40};
-    const unsigned char buffer_content_source_address[] = {0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa1};
-    const unsigned char buffer_content_destination_address[] = {0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa2};
-
-    buffer_t* buffer_ipv6_header = buffer_init(buffer_content_ipv6_header, IPV6_HEADER_LENGTH, RIGHT);
-
-    header_descriptor_t* header_descriptor_ipv6 = parse_ipv6_header(buffer_ipv6_header);
-
-    buffer_t* buffer_version = header_descriptor_ipv6->field_descriptors[0]->buffer;
-    buffer_t* buffer_traffic_class = header_descriptor_ipv6->field_descriptors[1]->buffer;
-    buffer_t* buffer_flow_label = header_descriptor_ipv6->field_descriptors[2]->buffer;
-    buffer_t* buffer_payload_length = header_descriptor_ipv6->field_descriptors[3]->buffer;
-    buffer_t* buffer_next_header = header_descriptor_ipv6->field_descriptors[4]->buffer;
-    buffer_t* buffer_hop_limit = header_descriptor_ipv6->field_descriptors[5]->buffer;
-    buffer_t* buffer_source_address = header_descriptor_ipv6->field_descriptors[6]->buffer;
-    buffer_t* buffer_destination_address = header_descriptor_ipv6->field_descriptors[7]->buffer;
-
-    assert(memcmp(buffer_version->content, buffer_content_version, 1) == 0);
-    assert(memcmp(buffer_traffic_class->content, buffer_content_traffic_class, 1) == 0);
-    assert(memcmp(buffer_flow_label->content, buffer_content_flow_label, 3) == 0);
-    assert(memcmp(buffer_payload_length->content, buffer_content_payload_length, 2) == 0);
-    assert(memcmp(buffer_next_header->content, buffer_content_next_header, 1) == 0);
-    assert(memcmp(buffer_hop_limit->content, buffer_content_hop_limit, 1) == 0);
-    assert(memcmp(buffer_source_address->content, buffer_content_source_address, 16) == 0);
-    assert(memcmp(buffer_destination_address->content, buffer_content_destination_address, 16) == 0);
-
-    buffer_deinit(buffer_ipv6_header);
-    header_descriptor_deinit(header_descriptor_ipv6);
-    return TEST_SUCCESS;
+  assert(ipv6_version == ipv6_expected_version);
+  assert(ipv6_traffic_class == ipv6_expected_traffic_class);
+  assert(ipv6_flow_label == ipv6_expected_flow_label);
 }
 
-int main(){
-    assert(test_ipv6_parser() == TEST_SUCCESS);
-    return TEST_SUCCESS;
+/* ********************************************************************** */
+
+void test_ipv6_parser(void) {
+  /**
+   * @brief Testing the IPv6 parser on an IPv6 header.
+   *
+   * The packet is made of an IPv6 header with following fields:
+   *  - id='Version'              len=4    pos=0  val={0x06}
+   *  - id='Traffic Class'        len=8    pos=0  val={0x00}
+   *  - id='Flow Label'           len=20   pos=0  val={0x00, 0x00, 0x00}
+   *  - id='Payload Length'       len=16   pos=0  val={0x10}
+   *  - id='Next Header'          len=8    pos=0  val={0x11}
+   *  - id='Hop Limit'            len=8    pos=0  val={0x40}
+   *  - id='Source Address'       len=128  pos=0  val={0xfe, 0x80, 0x00, 0x00,
+   * 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa1}
+   *  - id='Destination Address'  len=128  pos=0  val={0xfe, 0x80, 0x00, 0x00,
+   * 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa2}
+   */
+  const uint8_t ipv6_header[] = {
+      0x60, 0x00, 0x00, 0x00, 0x00, 0x10, 0x11, 0x40, 0xfe, 0x80, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa1,
+      0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0xa2, 0x23, 0x29, 0x23, 0x2a, 0x00, 0x10, 0x2d, 0xa1,
+      0x64, 0x65, 0x61, 0x64, 0x62, 0x65, 0x65, 0x66};
+
+  const uint8_t  ipv6_expected_version          = 0x06;
+  const uint8_t  ipv6_expected_traffic_class    = 0x00;
+  const uint32_t ipv6_expected_flow_label       = 0x000000;
+  const uint16_t ipv6_expected_payload_length   = 0x0010;
+  const uint8_t  ipv6_expected_next_header      = 0x11;
+  const uint8_t  ipv6_expected_hop_limit        = 0x40;
+  const uint8_t  ipv6_expected_source_address[] = {
+      0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa1};
+  const uint8_t ipv6_expected_destination_address[] = {
+      0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa2};
+
+  ipv6_hdr_t ipv6_parsed_header;
+  uint32_t   ipv6_parsed_ver_tc_fl;
+  uint8_t    ipv6_parsed_version;
+  uint8_t    ipv6_parsed_traffic_class;
+  uint32_t   ipv6_parsed_flow_label;
+  uint16_t   ipv6_parsed_payload_length;
+  uint8_t    ipv6_parsed_next_header;
+  uint8_t    ipv6_parsed_hop_limit;
+  uint8_t    ipv6_parsed_source_address[16];
+  uint8_t    ipv6_parsed_destination_address[16];
+
+  parse_ipv6_header(&ipv6_parsed_header, ipv6_header, 40);
+
+  // Version, Traffic Class and Flow Label
+  ipv6_parsed_ver_tc_fl = network_to_host_long(ipv6_parsed_header.ver_tc_fl);
+  unpack_ver_tc_fl(&ipv6_parsed_version, &ipv6_parsed_traffic_class,
+                   &ipv6_parsed_flow_label, ipv6_parsed_ver_tc_fl);
+  assert(ipv6_parsed_version == ipv6_expected_version);
+  assert(ipv6_parsed_traffic_class == ipv6_expected_traffic_class);
+  assert(ipv6_parsed_flow_label == ipv6_expected_flow_label);
+
+  // Payload Length
+  ipv6_parsed_payload_length =
+      network_to_host_short(ipv6_parsed_header.payload_length);
+  assert(ipv6_parsed_payload_length == ipv6_expected_payload_length);
+
+  // Next Header
+  ipv6_parsed_next_header = ipv6_parsed_header.next_header;
+  assert(ipv6_parsed_next_header == ipv6_expected_next_header);
+
+  // Hop Limit
+  ipv6_parsed_hop_limit = ipv6_parsed_header.hop_limit;
+  assert(ipv6_parsed_hop_limit == ipv6_expected_hop_limit);
+
+  // Source Address
+  memcpy(ipv6_parsed_source_address, ipv6_parsed_header.source_address, 16);
+  assert(memcmp(ipv6_parsed_source_address, ipv6_expected_source_address, 16) ==
+         0);
+
+  // Destination Address
+  memcpy(ipv6_parsed_destination_address,
+         ipv6_parsed_header.destination_address, 16);
+  assert(memcmp(ipv6_parsed_destination_address,
+                ipv6_expected_destination_address, 16) == 0);
+}
+
+/* ********************************************************************** */
+
+int main(void) {
+  test_unpack_ver_tc_fl();
+
+  test_ipv6_parser();
+
+  printf("All tests passed!\n");
+
+  return 0;
 }
--- test/test_udp_parser.c
+++ test/test_udp_parser.c
@@ -1,51 +1,114 @@
-#include <stdlib.h>
-#include <string.h>
+#include "parser/parser.h"
+
 #include <assert.h>
+#include <stdio.h>
+#include <string.h>
 
-#include "udp.h"
+/* ********************************************************************** */
 
-#define TEST_SUCCESS 0
+void test_udp_parser(void) {
+  /**
+   * @brief Testing the UDP parser on a UDP header.
+   *
+   * The packet is made of a UDP header with following fields:
+   *  - id='Source Port'          len=16  pos=0  value={0x23, 0x29}
+   *  - id='Destination Port'     len=16  pos=0  value={0x23, 0x2a}
+   *  - id='Length'               len=16  pos=0  value={0x00, 0x10}
+   *  - id='Checksum'             len=16  pos=0  value={0x2d, 0xa1}
+   *
+   */
+  const uint8_t udp_header[] = {
+      0x23, 0x29, 0x23, 0x2a, 0x00, 0x10, 0x2d, 0xa1,
+  };
 
-int test_udp_parser(){
-    /**
-     * @brief testing the UDP parser on a UDP header
-     * 
-     * The packet is made of a UDP header with following fields:
-     *  - id='Source Port'          length=4    position=0  value={0x23, 0x29}
-     *  - id='Destination Port'     length=8    position=0  value={0x23, 0x2a}
-     *  - id='Length'               length=20   position=0  value={0x00, 0x10}
-     *  - id='Checksum'             length=16   position=0  value={0x2d, 0xa1}
-     * 
-     */
-    const unsigned char buffer_content_udp_header[] = {
-        0x23, 0x29, 0x23, 0x2a, 0x00, 0x10, 0x2d, 0xa1,
-    };
+  const uint16_t udp_expected_source_port      = 0x2329;
+  const uint16_t udp_expected_destination_port = 0x232a;
+  const uint16_t udp_expected_length           = 0x0010;
+  const uint16_t udp_expected_checksum         = 0x2da1;
 
-    const unsigned char buffer_content_source_port[] = {0x23, 0x29};
-    const unsigned char buffer_content_destination_port[] = {0x23, 0x2a};
-    const unsigned char buffer_content_length[] = {0x00, 0x10};
-    const unsigned char buffer_content_checksum[] = {0x2d, 0xa1};
-    
-    buffer_t* buffer_udp_header = buffer_init(buffer_content_udp_header, UDP_HEADER_LENGTH, RIGHT);
+  udp_hdr_t udp_parsed_header;
+  uint16_t  udp_parsed_source_port;
+  uint16_t  udp_parsed_destination_port;
+  uint16_t  udp_parsed_length;
+  uint16_t  udp_parsed_checksum;
 
-    header_descriptor_t* header_descriptor_udp = parse_udp_header(buffer_udp_header);
+  parse_udp_header(&udp_parsed_header, udp_header, 0, 8);
 
-    buffer_t* buffer_source_port = header_descriptor_udp->field_descriptors[0]->buffer;
-    buffer_t* buffer_destination_port = header_descriptor_udp->field_descriptors[1]->buffer;
-    buffer_t* buffer_length = header_descriptor_udp->field_descriptors[2]->buffer;
-    buffer_t* buffer_checksum = header_descriptor_udp->field_descriptors[3]->buffer;
-    
-    assert(memcmp(buffer_source_port->content, buffer_content_source_port, 2) == 0);
-    assert(memcmp(buffer_destination_port->content, buffer_content_destination_port, 2) == 0);
-    assert(memcmp(buffer_length->content, buffer_content_length, 2) == 0);
-    assert(memcmp(buffer_checksum->content, buffer_content_checksum, 2) == 0);
-    
-    buffer_deinit(buffer_udp_header);
-    header_descriptor_deinit(header_descriptor_udp);
-    return TEST_SUCCESS;
+  // Source Port
+  udp_parsed_source_port = network_to_host_short(udp_parsed_header.source_port);
+  assert(udp_parsed_source_port == udp_expected_source_port);
+
+  // Destination Port
+  udp_parsed_destination_port =
+      network_to_host_short(udp_parsed_header.destination_port);
+  assert(udp_parsed_destination_port == udp_expected_destination_port);
+
+  // Length
+  udp_parsed_length = network_to_host_short(udp_parsed_header.length);
+  assert(udp_parsed_length == udp_expected_length);
+
+  // Checksum
+  udp_parsed_checksum = network_to_host_short(udp_parsed_header.checksum);
+  assert(udp_parsed_checksum == udp_expected_checksum);
 }
 
-int main(){
-    assert(test_udp_parser() == TEST_SUCCESS);
-    return TEST_SUCCESS;
+/* ********************************************************************** */
+
+void test_udp_parser_from_packet(void) {
+  /**
+   * @brief Testing the UDP parser on an IPv6 and UDP header.
+   *
+   * The packet is made of a UDP header with following fields:
+   *  - id='Source Port'          len=16  pos=0  value={0x1f, 0x90}
+   *  - id='Destination Port'     len=16  pos=0  value={0x00, 0x35}
+   *  - id='Length'               len=16  pos=0  value={0x00, 0x14}
+   *  - id='Checksum'             len=16  pos=0  value={0x00, 0x00}
+   *
+   */
+  const uint8_t ipv6_udp_packet[] = {
+      0x60, 0x00, 0x00, 0x00, 0x00, 0x14, 0x11, 0x40, 0xfe, 0x80, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x5e, 0xff, 0xfe, 0x00, 0x00, 0x01,
+      0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x5e, 0xff,
+      0xfe, 0x00, 0x00, 0x02, 0x1f, 0x90, 0x00, 0x35, 0x00, 0x14, 0x00, 0x00};
+
+  const uint16_t udp_expected_source_port      = 0x1f90;
+  const uint16_t udp_expected_destination_port = 0x0035;
+  const uint16_t udp_expected_length           = 0x0014;
+  const uint16_t udp_expected_checksum         = 0x0000;
+
+  udp_hdr_t udp_parsed_header;
+  uint16_t  udp_parsed_source_port;
+  uint16_t  udp_parsed_destination_port;
+  uint16_t  udp_parsed_length;
+  uint16_t  udp_parsed_checksum;
+
+  parse_udp_header(&udp_parsed_header, ipv6_udp_packet, 40, 48);
+
+  // Source Port
+  udp_parsed_source_port = network_to_host_short(udp_parsed_header.source_port);
+  assert(udp_parsed_source_port == udp_expected_source_port);
+
+  // Destination Port
+  udp_parsed_destination_port =
+      network_to_host_short(udp_parsed_header.destination_port);
+  assert(udp_parsed_destination_port == udp_expected_destination_port);
+
+  // Length
+  udp_parsed_length = network_to_host_short(udp_parsed_header.length);
+  assert(udp_parsed_length == udp_expected_length);
+
+  // Checksum
+  udp_parsed_checksum = network_to_host_short(udp_parsed_header.checksum);
+  assert(udp_parsed_checksum == udp_expected_checksum);
+}
+
+/* ********************************************************************** */
+
+int main() {
+  test_udp_parser();
+  test_udp_parser_from_packet();
+
+  printf("All tests passed!\n");
+
+  return 0;
 }
