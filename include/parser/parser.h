637d19ea4a411cd1836986c43aa1137edf5952bf
Corentin Banier <corentinbanier@protonmail.com>
2024-07-17 16:40:07 +0200

    feature(parser and yang): Remove useless functions and corresponding tests.

--- include/parser/parser.h
+++ include/parser/parser.h
@@ -4,4 +4,12 @@
 #include "parser/protocol/ipv6.h"
 #include "parser/protocol/udp.h"
 
+/**
+ * @brief Enumeration of supported protocols.
+ */
+typedef enum {
+  PROTOCOL_IPV6,
+  PROTOCOL_UDP,
+} protocol_t;
+
 #endif  // _PARSER_H_
--- include/parser/yang_handler.h
+++ include/parser/yang_handler.h
@@ -7,19 +7,10 @@
 #include <stdint.h>
 
 /**
- * @brief Enumeration of supported protocols.
- */
-typedef enum {
-  PROTOCOL_IPV6,
-  PROTOCOL_UDP,
-} protocol_t;
-
-/**
  * @brief Structure which map protocol fields to their FIDs.
  */
 typedef struct {
   protocol_t protocol;
-  int        offset;
   uint8_t    byte_length;
   uint16_t   fid;
 } field_fid_mapping_t;
@@ -29,54 +20,20 @@ typedef struct {
  */
 static const field_fid_mapping_t field_fid_mappings[] = {
   // IPv6 mappings
-    {PROTOCOL_IPV6, offsetof(ipv6_hdr_t, version),             IPV6_VERSION_BYTE_LENGTH,
-     FID_IPV6_VERSION                                                                                             },
-    {PROTOCOL_IPV6, offsetof(ipv6_hdr_t, traffic_class),
-     IPV6_TRAFFIC_CLASS_BYTE_LENGTH,                                                       FID_IPV6_TRAFFIC_CLASS },
-    {PROTOCOL_IPV6, offsetof(ipv6_hdr_t, flow_label),
-     IPV6_FLOW_LABEL_BYTE_LENGTH,                                                          FID_IPV6_FLOW_LABEL    },
-    {PROTOCOL_IPV6, offsetof(ipv6_hdr_t, payload_length),
-     IPV6_PAYLOAD_LENGTH_BYTE_LENGTH,                                                      FID_IPV6_PAYLOAD_LENGTH},
-    {PROTOCOL_IPV6, offsetof(ipv6_hdr_t, next_header),
-     IPV6_NEXT_HEADER_BYTE_LENGTH,                                                         FID_IPV6_NEXT_HEADER   },
-    {PROTOCOL_IPV6, offsetof(ipv6_hdr_t, hop_limit),           IPV6_HOP_LIMIT_BYTE_LENGTH,
-     FID_IPV6_HOP_LIMIT                                                                                           },
-    {PROTOCOL_IPV6, offsetof(ipv6_hdr_t, source_address),
-     IPV6_SRC_ADDRESS_BYTE_LENGTH,                                                         FID_IPV6_SRC_ADDRESS   },
-    {PROTOCOL_IPV6, offsetof(ipv6_hdr_t, destination_address),
-     IPV6_DST_ADDRESS_BYTE_LENGTH,                                                         FID_IPV6_DST_ADDRESS   },
+    {PROTOCOL_IPV6, IPV6_VERSION_BYTE_LENGTH,        FID_IPV6_VERSION       },
+    {PROTOCOL_IPV6, IPV6_TRAFFIC_CLASS_BYTE_LENGTH,  FID_IPV6_TRAFFIC_CLASS },
+    {PROTOCOL_IPV6, IPV6_FLOW_LABEL_BYTE_LENGTH,     FID_IPV6_FLOW_LABEL    },
+    {PROTOCOL_IPV6, IPV6_PAYLOAD_LENGTH_BYTE_LENGTH, FID_IPV6_PAYLOAD_LENGTH},
+    {PROTOCOL_IPV6, IPV6_NEXT_HEADER_BYTE_LENGTH,    FID_IPV6_NEXT_HEADER   },
+    {PROTOCOL_IPV6, IPV6_HOP_LIMIT_BYTE_LENGTH,      FID_IPV6_HOP_LIMIT     },
+    {PROTOCOL_IPV6, IPV6_SRC_ADDRESS_BYTE_LENGTH,    FID_IPV6_SRC_ADDRESS   },
+    {PROTOCOL_IPV6, IPV6_DST_ADDRESS_BYTE_LENGTH,    FID_IPV6_DST_ADDRESS   },
 
  // UDP mappings
-    {PROTOCOL_UDP,  offsetof(udp_hdr_t,  app_port),            UDP_APP_PORT_BYTE_LENGTH,
-     FID_UDP_APP_PORT                                                                                             },
-    {PROTOCOL_UDP,  offsetof(udp_hdr_t,  dev_port),            UDP_DEV_PORT_BYTE_LENGTH,
-     FID_UDP_DEV_PORT                                                                                             },
-    {PROTOCOL_UDP,  offsetof(udp_hdr_t,  length),              UDP_LENGTH_BYTE_LENGTH,
-     FID_UDP_LENGTH                                                                                               },
-    {PROTOCOL_UDP,  offsetof(udp_hdr_t,  checksum),            UDP_CHECKSUM_BYTE_LENGTH,
-     FID_UDP_CHECKSUM                                                                                             },
+    {PROTOCOL_UDP,  UDP_APP_PORT_BYTE_LENGTH,        FID_UDP_APP_PORT       },
+    {PROTOCOL_UDP,  UDP_DEV_PORT_BYTE_LENGTH,        FID_UDP_DEV_PORT       },
+    {PROTOCOL_UDP,  UDP_LENGTH_BYTE_LENGTH,          FID_UDP_LENGTH         },
+    {PROTOCOL_UDP,  UDP_CHECKSUM_BYTE_LENGTH,        FID_UDP_CHECKSUM       },
 };
-
-/**
- * @brief Get the FID by protocol and offset.
- *
- * @param protocol The protocol type.
- * @param offset The offset of the field within the protocol header.
- * @return The FID of the field, or 0 if no matching FID is found.
- */
-uint16_t get_fid_by_protocol_and_offset(const protocol_t protocol,
-                                        const int        offset);
-
-/**
- * @brief Get the protocol and offset by FID.
- *
- * @param fid The Field Identifier (FID).
- * @param protocol Pointer to store the protocol type.
- * @param offset Pointer to store the offset of the field within the protocol
- * header.
- * @return 1 if success, 0 if no matching FID is found.
- */
-int get_protocol_and_offset_by_fid(protocol_t* protocol, int* offset,
-                                   const uint16_t fid);
 
 #endif  // _YANG_HANDLER_H_
