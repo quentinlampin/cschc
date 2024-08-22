6ece20469b83846f9b2e3b471c8054d3f02adb8c
Corentin Banier <corentinbanier@protonmail.com>
2024-08-22 10:37:31 +0200

    feature(context): Add a function to determine the number of CDA_COMPUTE in a Rule Descriptor.

--- include/core/context.h
+++ include/core/context.h
@@ -2,16 +2,15 @@
 #define _CONTEXT_H_
 
 /**
- * @brief A context is defined as a byte array.
+ * @brief In CSCHC, a SCHC Context is defined as a byte array.
  *
- * @details According to RFC 8724, a context consists of various compression
- * rules. Each rule is composed of rule field descriptors, which may or may not
- * include one or multiple target values.
+ * @details According to RFC 8724, a Context consists of various compression
+ * Rule Descriptors. Each Rule Descriptor is composed of Rule Field Descriptors,
+ * which may or may not include one or multiple Target Values.
  *
- * @note A target value of a rule field descriptor can appear in different
- * places. Same idea for a rule field descriptor and different rule descriptor.
- * Therefore, the context byte array stores the offset of each object
- * represented.
+ * @note A Target Value of a Rule Field Descriptor can appear in different
+ * places. Same idea for a Rule Field Descriptor. Therefore, the Context byte
+ * array stores the offset of each object represented.
  *
  * @struct
  * // Context
@@ -44,5 +43,32 @@
 #include "rule_descriptor.h"
 #include "rule_field_descriptor.h"
 #include "schc8724.h"
+
+/**
+ * @brief Struct that stores the current bit position of a Rule Field Descriptor
+ * which uses CDA_COMPUTE along with its position in the Rule Descriptor.
+ */
+typedef struct {
+  size_t bit_position;
+  int    index_rule_field_descriptor;
+} compute_entry_t;
+
+/**
+ * @brief Get the number of CDA fields that are equal to CDA_Compute among all
+ * Rule Field Descriptors in a Rule Descriptor.
+ *
+ * @details As specified in SCHC RFC 8724, only a few fields can use the Compute
+ * action. However, we can still use the Value-Sent action on these fields. This
+ * function ensures that we know the correct number of fields to compute before
+ * applying the compression, even though this function adds O(n) complexity.
+ *
+ * @param rule_descriptor Pointer to the rule descriptor used for decompression.
+ * @param context Pointer to the SCHC Context used to perform decompression.
+ * @param context_byte_len Byte length of the context.
+ * @return The number of fields that use the Compute action.
+ */
+int get_cardinal_compute_entries(const rule_descriptor_t *rule_descriptor,
+                                 const uint8_t           *context,
+                                 const size_t             context_byte_len);
 
 #endif  // _CONTEXT_H_
